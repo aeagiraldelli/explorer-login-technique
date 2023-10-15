437c8f8efe4fb397029f7bc4c9cbc90b86566ae7
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-10-15 11:54:00 -0300

    front-end send authorization cookie.

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,10 +1,11 @@
+import { useEffect } from 'react';
 import { Container } from "./styles";
 
 export function SalesReport() {
+
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
     </Container>
-
-  )
+  );
 }
